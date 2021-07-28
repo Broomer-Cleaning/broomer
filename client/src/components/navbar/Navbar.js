ece5ffe8b0859b1ff3a92abe1848cf6606611708
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-07-28 10:13:45 -0400

    logo added, css for nav

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./navbar.css";
-// import logo from "../logo.png";
+import logo from "./img/logo.png";
 import {Link} from "react-scroll";
-// import resume from "../images/Full-Stack-Developer.pdf"
+
 
 //react font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -14,7 +14,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
 
-    {/* <Link smooth={true} to="home" className="navbar-brand"><img className="logo" src={logo} alt="logo"></img></Link> */}
+    <Link smooth={true} to="home" className="navbar-brand"><img className="logo" src={logo} alt="logo"></img></Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <FontAwesomeIcon icon={faBars} style ={{color: "#fff"}}/>
     </button>
@@ -25,16 +25,16 @@ const Navbar = () => {
           <Link smooth={true} to="home" className="nav-link">Home <span className="sr-only"></span></Link>
         </li>
         <li className="nav-item">
-          <Link smooth={true} to="aboutMe" offset={-110} className="nav-link">about me</Link>
+          <Link smooth={true} to="aboutMe" offset={-110} className="nav-link">about us</Link>
         </li> 
         <li className="nav-item">
-          <Link smooth={true} to="portfolio" offset={-110} className="nav-link">portfolio</Link>
+          <Link smooth={true} to="aboutMe" offset={-110} className="nav-link">services</Link>
         </li> 
-        <li className="nav-item active">
-          <a className="nav-link" href="#" target="_blank" rel="noopener noreferrer">Resume <span className="sr-only"></span></a>
-        </li>
         <li className="nav-item">
-          <Link smooth={true} to="contactMe" offset={-110} className="nav-link">contact me</Link>
+          <Link smooth={true} to="portfolio" offset={-110} className="nav-link">contact us</Link>
+        </li> 
+        <li className="nav-item">
+          <Link smooth={true} to="contactMe" offset={-110} className="nav-link">Login</Link>
         </li> 
       </ul>
     </div>
