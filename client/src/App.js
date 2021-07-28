070811eef79ad0b4337dac165b2739f5deebe79e
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-07-27 20:32:30 -0400

    nav bar added, linked

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import Navbar from './components/Navbar';
+import ApolloClient from 'apollo-boost';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+    <>
+     <Navbar/>
+     </>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "../logo.png";
+// import logo from "../logo.png";
 import {Link} from "react-scroll";
-import resume from "../images/Full-Stack-Developer.pdf"
+// import resume from "../images/Full-Stack-Developer.pdf"
 
 //react font awesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faBars} from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {faBars} from "@fortawesome/free-solid-svg-icons";
 
 
 const Navbar = () => {
@@ -13,9 +13,9 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
       <div className="container">
 
-    <Link smooth={true} to="home" className="navbar-brand"><img className="logo" src={logo} alt="logo"></img></Link>
+    {/* <Link smooth={true} to="home" className="navbar-brand"><img className="logo" src={logo} alt="logo"></img></Link> */}
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <FontAwesomeIcon icon={faBars} style ={{color: "#fff"}}/>
+      {/* <FontAwesomeIcon icon={faBars} style ={{color: "#fff"}}/> */}
     </button>
   
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -30,7 +30,7 @@ const Navbar = () => {
           <Link smooth={true} to="portfolio" offset={-110} className="nav-link">portfolio</Link>
         </li> 
         <li className="nav-item active">
-          <a className="nav-link" href={resume} target="_blank" rel="noopener noreferrer">Resume <span className="sr-only"></span></a>
+          <a className="nav-link" href="#" target="_blank" rel="noopener noreferrer">Resume <span className="sr-only"></span></a>
         </li>
         <li className="nav-item">
           <Link smooth={true} to="contactMe" offset={-110} className="nav-link">contact me</Link>
