2c86a25d0504817431719926b1594e7db424a7df
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-07-29 11:31:19 -0400

    app.js carousel removed

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import Dashboard from './components/dashboard/Dashboard';
 import Header from './components/header/Header';
 import AboutUs from './components/about/AboutUs';
 import Reviews from './components/reviews/Reviews';
-import ReviewsCarousel from './components/reviews/ReviewsCarousel';
 import Footer from './components/footer/Footer';
 import ApolloClient from 'apollo-boost';
 import "bootstrap/dist/css/bootstrap.min.css";
