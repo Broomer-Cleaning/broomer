162a6a6eb6b12012aab900a3197e454656b3d008
F34r Th3 R34per <danielpisani12@hotmail.com>
2021-07-29 21:22:45 -0400

    added User-profile related mutation. Working on 'Add a job' mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,17 +20,17 @@ const typeDefs = gql`
         jobs_hired: [Job]
     }
 
-    input profileSetup {
-        first_name: String
-        last_name: String
-        date_of_birth: String
-        phone_number: String
-        about_me: String
-        safety_double_vax: Boolean
-        safety_mask: Boolean
-        safety_police_check: Boolean
-        have_pets: Boolean
-    }
+    # input profileSetup {
+    #     first_name: String
+    #     last_name: String
+    #     date_of_birth: String
+    #     phone_number: String
+    #     about_me: String
+    #     safety_double_vax: Boolean
+    #     safety_mask: Boolean
+    #     safety_police_check: Boolean
+    #     have_pets: Boolean
+    # }
 
     type Job {
         _id: ID!
@@ -69,11 +69,14 @@ const typeDefs = gql`
         est_hours: Float
         rate_per_hour: Float
         job_description: String
-        # safety_double_vax: Boolean
-        # safety_mask: Boolean
-        # safety_police_check: Boolean
-        # have_pets: Boolean
-        # need_supplies_worker: Boolean
+        safety_double_vax: Boolean
+        safety_mask: Boolean
+        safety_police_check: Boolean
+        have_pets: Boolean
+        need_supplies_worker: Boolean
+        emp_provides_meal: Boolean
+        emp_provides_drinks: Boolean
+        emp_provides_facilities: Boolean
     }
 
     type Review {
@@ -101,10 +104,22 @@ const typeDefs = gql`
         createUser(username: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!): Auth
         
-        profileDetails(userData: profileSetup!): User
-        # addAJob(street_address: String!, postal_code: String!): Job
-        
-        addAJob(jobData: JobDetails!): Job
+        # profileDetails(userData: profileSetup!): User
+
+        profileDetails(first_name: String, last_name: String, 
+        date_of_birth: String,
+        phone_number: String,
+        about_me: String,
+        safety_double_vax: Boolean,
+        safety_mask: Boolean,
+        safety_police_check: Boolean,
+        have_pets: Boolean
+        ): User
+
+        # Start here with 'addAJob' mutation
+        addAJob(street_address: String!, postal_code: String!): Job
+
+        # addAJob(jobData: JobDetails!): Job
     }
 `
 
