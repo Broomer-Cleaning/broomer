59529402975c9f8aa9ebe76fd8b77860b6cde0ff
ahmed hakeem <hakeemkoo@hotmail.com>
2021-07-31 16:22:40 -0400

    dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,33 @@
 import "./dashboard.css";
 import React from "react";
-import Calendar from "./calendar/calendar";
 import List from "./list/List";
-import { Button, Container } from "react-bootstrap";
-
+import { Button, Container, Nav } from "react-bootstrap";
 
 const Dashboard = () => {
+  const handleCilck = () => {
+    console.log("Hello Ahmed");
+  };
+
   return (
     <div className="main">
+      <Container className="boxHedaer">
+        <div className="postJobBox">
+          <h4>Total of Jod Posted</h4>
+          <p> Jods</p>
+        </div>
+        <div className="compeletJobBox">
+          <h4>Total of Jod Completed</h4>
+          <p> Jods</p>
+        </div>
+        <div className="incomBox">
+          <h4> Total of Income</h4>
+          <p> $</p>
+        </div>
+        <div className="incomBox">
+          <h4> Total of Outcome</h4>
+          <p> $</p>
+        </div>
+      </Container>
       <div className="rightSide">
         <div className="medSection">
           <div className="listview">
@@ -21,36 +41,32 @@ const Dashboard = () => {
             <div className="postJobBox">
               <h4>Jod Posted</h4>
               <div className="JobList">
-              <ul>
-                <li>Job Tital <span><Button variant="warning">Edit Job</Button> <Button variant="danger">Delete Job</Button> <Button variant="secondary">Done</Button></span></li>
-                <li>Job Tital <span><Button variant="warning">Edit Job</Button> <Button variant="danger">Delete Job</Button> <Button variant="secondary">Done</Button></span></li>
-                <li>Job Tital <span><Button variant="warning">Edit Job</Button> <Button variant="danger">Delete Job</Button> <Button variant="secondary">Done</Button></span></li>
-                <li>Job Tital <span><Button variant="warning">Edit Job</Button> <Button variant="danger">Delete Job</Button> <Button variant="secondary">Done</Button></span></li>
-                <li>Job Tital <span><Button variant="warning">Edit Job</Button> <Button variant="danger">Delete Job</Button> <Button variant="secondary">Done</Button></span></li>
-              </ul>
+                <ul>
+                  <li>
+                    Job Tital{" "}
+                    <span>
+                      <Button onClick={handleCilck} variant="warning">
+                        Edit Job
+                      </Button>{" "}
+                      <Button variant="danger">Delete Job</Button>{" "}
+                      <Button variant="secondary">Done</Button>
+                    </span>
+                  </li>
+                </ul>
               </div>
               <Button>Add Job</Button>
             </div>
             <div className="compeletJobBox">
               <h4>Jod Completed</h4>
               <div className="JobList">
-              <ul>
-                <li>Job Tital <span><Button variant="info">Write Review</Button></span></li>
-                <li>Job Tital <span><Button variant="info">Write Review</Button></span></li>
-                <li>Job Tital <span><Button variant="info">Write Review</Button></span></li>
-                <li>Job Tital <span><Button variant="info">Write Review</Button></span></li>
-              </ul>
-              </div>
-            </div>
-            <div className="incomBox">
-              <h4>Income</h4>
-              <div className="JobList">
-              <ul>
-                <li>Job Tital: $ </li>
-                <li>Job Tital: $ </li>
-                <li>Job Tital: $ </li>
-                <li>Job Tital: $ </li>
-              </ul>
+                <ul>
+                  <li>
+                    Job Tital{" "}
+                    <span>
+                      <Button variant="info">Write Review</Button>
+                    </span>
+                  </li>
+                </ul>
               </div>
             </div>
           </Container>
