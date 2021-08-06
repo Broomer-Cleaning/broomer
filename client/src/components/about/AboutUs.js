9bf54ed3a236ec9d5a4c0214596e4d56509290ef
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-08-05 21:56:32 -0400

    about me css

--- client/src/components/about/AboutUs.js
+++ client/src/components/about/AboutUs.js
@@ -16,16 +16,24 @@ const AboutUs = () => {
           <p>
             Broomer is an instant job networking application for connecting cleaners and people who need quick cleaning jobs. If you have a messy house, car, garage, office, or other kind of space that needs a little extra care, leave it to Broomer to introduce you to dozens of qualified cleaning professionals.
           </p>
-            <h2>How It Works (and how to get started in less than three minutes)!</h2>
-            <h3>If you need a cleaning professional..</h3>
-            <p>Create an account, fill out your profile, then create a job. Give us a few simple details, including some estimates for time required and pay per hour. When you hit publish, your job will be live and is instantly available to dozens of cleaning professionals. The first one who applies for the job, while matching your credentials, will be deployed to your home. It's that fast!</p>
+          <br />
+          <h3>How It Works (and how to get started in less than three minutes)!</h3>
+          <br />
+          <h4>If you need a cleaning professional..</h4>
+          <br />
+          <p>Create an account, fill out your profile, then create a job. Give us a few simple details, including some estimates for time required and pay per hour. When you hit publish, your job will be live and is instantly available to dozens of cleaning professionals. The first one who applies for the job, while matching your credentials, will be deployed to your home. It's that fast!</p>
 
-            <p>After they're done, they submit their work for review. You must then confirm completion of work. After that, you get to confirm the work details. Tipping doesn't hurt, either!</p>
 
-            <h3>If you are a cleaning professional..</h3>
-            <p>Sign up, then create an account. Your account details can matter, as some employers prefer that you meet certain safety requirements. For example, if you don't have a recently valid police check, then you won't have access to jobs which demand that.</p>
+          <p>After they're done, they submit their work for review. You must then confirm completion of work. After that, you get to confirm the work details. Tipping doesn't hurt, either!</p>
+          <br />
 
-            <p>After publishing your profile, you can begin applying to jobs in your area. If you apply, expect to head there as soon as possible; Broomer is about providing instant service!</p>
+          <h4>If you are a cleaning professional..</h4>
+          <br />
+          <p>Sign up, then create an account. Your account details can matter, as some employers prefer that you meet certain safety requirements. For example, if you don't have a recently valid police check, then you won't have access to jobs which demand that.</p>
+
+
+          <p>After publishing your profile, you can begin applying to jobs in your area. If you apply, expect to head there as soon as possible; Broomer is about providing instant service!</p>
+          <br />
         </div>
       </div>
 
