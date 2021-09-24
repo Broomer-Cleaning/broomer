93f9b7ccd43f421709bb93a7041d49b258ea987b
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-09-24 19:24:40 -0400

    invalid token fixed

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import "./login.css";
-import { Form, Button, Alert} from 'react-bootstrap';
+import { Form, Button, Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import Auth from '../../utils/auth';
@@ -31,7 +31,7 @@ const LoginForm = () => {
 
     try {
       const { data } = await login({ variables: { ...userFormData } });
-      console.log(data);
+      console.log(data.login.token);
       Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
@@ -45,53 +45,53 @@ const LoginForm = () => {
     });
   };
 
-  
+
 
   return (
     <>
-    <div className="card mx-auto">
-    <h2 className="text-center mb-4">login</h2>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
-        </Alert>
-        <Form.Group>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your email'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
+      <div className="card mx-auto">
+        <h2 className="text-center mb-4">login</h2>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+            Something went wrong with your login credentials!
+          </Alert>
+          <Form.Group>
+            <Form.Label htmlFor='email'>Email</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Your email'
+              name='email'
+              onChange={handleInputChange}
+              value={userFormData.email}
+              required
+            />
+            <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.email && userFormData.password)} 
-          type='submit'
-          variant='success'>
-          Submit
-        </Button>
-      </Form>
+          <Form.Group>
+            <Form.Label htmlFor='password'>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Your password'
+              name='password'
+              onChange={handleInputChange}
+              value={userFormData.password}
+              required
+            />
+            <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+          </Form.Group>
+          <Button
+            disabled={!(userFormData.email && userFormData.password)}
+            type='submit'
+            variant='success'>
+            Submit
+          </Button>
+        </Form>
       </div>
       <div className=" question w-100 text-center mt-2">Don't have an account? <Link to="signup" className="nav-link">Sign up</Link></div>
-      
+
     </>
-    
+
   );
 };
 
